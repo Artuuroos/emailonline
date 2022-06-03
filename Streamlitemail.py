6570f33fa63411a7a755fef961623619112b2892
Artuuroos <106031033+Artuuroos@users.noreply.github.com>
2022-06-03 10:34:50 +0200

    Update Streamlitemail.py

--- Streamlitemail.py
+++ Streamlitemail.py
@@ -11,7 +11,7 @@ message = """\
 Subject: Neuer Preis DB 
 
 
-Ein neuer Preis ihrer Verbindung ist verfügbar,kaufen Sie sich ein Ticket."""
+Ein neuer Preis ihrer Verbindung ist verfügbar kaufen Sie sich ein Ticket."""
 
 
 zahl=st.number_input("Zahl: ",min_value=1,max_value=11,step=1) 
