078669a9b9c0698932427eb27790be2c833fa582
Artuuroos <106031033+Artuuroos@users.noreply.github.com>
2022-06-05 14:28:40 +0200

    Update Streamlitemail.py

--- Streamlitemail.py
+++ Streamlitemail.py
@@ -1,52 +1,126 @@
-import smtplib, ssl
+from turtle import color
+import streamlit as st
+import plotly.figure_factory as ff
+import numpy as np
 import pandas as pd
-import streamlit as st 
+import plotly.express as px
+from streamlit_option_menu import option_menu
+import smtplib, ssl
 import requests
 import streamlit as st
 import time
 
 
 
-st.subheader("Benachrichtigung anfordern")
-emailteil1=st.text_input("Emailnamen eingeben")
-emaildomains=["@gmail.com","@gmx.de","@web.de"]
 
-option = st.selectbox('Email Domain auswählen', emaildomains)
-ganzeemail=emailteil1+option
+optionen2 = option_menu(menu_title=None,
+                       options=["Home","Diagramm","Price","Notifi"],
+                       icons=["house","graph-up","clock","alarm"],
+                       menu_icon="cast",
+                       default_index=0,
+                       orientation="horizontal",
+                       )
 
-port = 587  # For starttls
-smtp_server = "smtp.gmail.com"
-sender_email = "dbtickeralert@gmail.com"
-receiver_email = ganzeemail
-password = "ujbdfkbgqwbjemrh"
-message = """\
-Subject: Neuer Preis DB 
 
-Ein neuer Preis ihrer Verbindung ist verfuegbar.
-Kaufen Sie sich ein Ticket.
+if optionen2=="Home":
+    st.write("Homepage")
+    
+else:
+    if optionen2=="Diagramm":
+        with st.sidebar:
+            optionenside= option_menu(menu_title=None,
+            options=["Liniendiagramm","Säulendiagramm"],
+            icons=["graph-up","bar-chart-line"],
+            menu_icon="",
+            default_index=0,
+            orientation="vertical",
+        )
+        if optionenside=="Liniendiagramm":
+            st.write("Hier kommt ein Liniendiagramm hin")
+            with st.form(key='form1'):
+                submit_button = st.form_submit_button(label='Graph ansehen')
+            if submit_button:
+                fig = px.line(        
+                df, #Data Frame
+                x = "Scan", #Columns from the data frame
+                y = "Preis",
+                title = "Preisgraph"
+            )
+                fig.update_traces(line_color = "skyblue")
+                st.plotly_chart(fig)
+           
+        else:
+            st.write("Hier kommt ein Säulendiagramm hin")
+            with st.form(key='form1'):
+                submit_button1 = st.form_submit_button(label='Graph ansehen')
+            if submit_button1:
+                fig = px.bar(        
+                df, #Data Frame
+                x = "Scan", #Columns from the data frame
+                y = "Preis",
+                title = "Preisgraph"
+            )
+                fig.update_traces(line_color = "skyblue")
+                st.plotly_chart(fig) 
+       
+        
+    if optionen2=="Price":
+        st.write("Preisantizipation")
+        
+    if optionen2=="Notifi":
+        
+        st.subheader("Benachrichtigung anfordern")
+        emailteil1=st.text_input("Emailnamen eingeben")
+        emaildomains=["@gmail.com","@gmx.de","@web.de"]
 
-Freundlicher Gruss
-DBTickeralert"""
+        option = st.selectbox('Email Domain auswählen', emaildomains)
+        ganzeemail=emailteil1+option
 
-zahl=st.number_input("Preis: ",min_value=1,max_value=11,step=1) 
-st.write("Ihr angegebener Preis: ", zahl,"€")
-liste=[1,2,3,4,5,6,7,8,9,10]
+        port = 587  # For starttls
+        smtp_server = "smtp.gmail.com"
+        sender_email = "dbtickeralert@gmail.com"
+        receiver_email = ganzeemail
+        password = "ujbdfkbgqwbjemrh"
+        message = """\
+        Subject: Neuer Preis DB 
 
-with st.form(key='form1'):
-    submit_button = st.form_submit_button(label='Email anfordern')
+        Ein neuer Preis ihrer Verbindung ist verfuegbar.
+        Kaufen Sie sich ein Ticket.
+
+        Freundlicher Gruss
+        DBTickeralert"""
+
+        liste=[1,2,3,4,5,6,7,8,9,10]
+
+                
+        preisangabe = st.slider("Ihr gewünschter Höchstpreis:")
+        with st.form(key='form1'):
+                submit_buttonpreis = st.form_submit_button(label='Benachrichtige mich')    
+                if submit_buttonpreis:
+                    st.write("Sie erhalten eine Email Benachrichitung wenn sich der Preis unter",preisangabe ,"€ befindet") 
+        context = ssl.create_default_context()
+        with smtplib.SMTP(smtp_server, port) as server:
+            server.ehlo()  # Can be omitted
+            server.starttls(context=context)
+            server.ehlo()  # Can be omitted
+            server.login(sender_email, password)
+            for i in range(len(liste)):
+                if liste[i]<=preisangabe:
+                    server.sendmail(sender_email, receiver_email, message)
+                else:
+                    if preisangabe>liste[i]:
+                        st.write("Ihre Kaufbereitschaft ist sehr hoch") 
+           
+                      
+        
+
+
+
     
    
-if submit_button:
-    context = ssl.create_default_context()
-    with smtplib.SMTP(smtp_server, port) as server:
-        server.ehlo()  # Can be omitted
-        server.starttls(context=context)
-        server.ehlo()  # Can be omitted
-        server.login(sender_email, password)
-        for i in range(len(liste)):
-            if liste[i]<=zahl:
-                server.sendmail(sender_email, receiver_email, message)
-            else:
-                if zahl>liste[i]:
-                    st.write("Es gibt kein Ticket für diesen Preis")
+        
+
+        
+
+
 
