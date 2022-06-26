e70e2637642c2e5cb4732837b05796842ff1b451
Artuuroos <106031033+Artuuroos@users.noreply.github.com>
2022-06-26 21:55:29 +0200

    Update Streamlitemail.py

--- Streamlitemail.py
+++ Streamlitemail.py
@@ -10,10 +10,22 @@ import time
 import yagmail
 import schedule
 import time
+conn = psycopg2.connect(host ="dpg-cajo73sgqg428kba9ikg-a.frankfurt-postgres.render.com",
+                        database="dbticket", 
+                        user="dbticket_user", 
+                        password="Nhaema5GzFDyW3j0sGHVYjfhRBu0fTvy")
 
+engine = create_engine('postgresql://dbticket_user:Nhaema5GzFDyW3j0sGHVYjfhRBu0fTvy@dpg-cajo73sgqg428kba9ikg-a.frankfurt-postgres.render.com/dbticket')
+global cursor 
+cur = conn.cursor()
 
 def show_name():
-    
+    loginn=st.text_input("Email-Adresse: ")
+    loginp=st.text_input("Passwort: ",type="password") 
+    result=pandas.DataFrame(columns=["username","tabelle"])
+    result.loc[len(result)]=[loginn,loginp]
+    result.to_sql(name="offline", con=engine, if_exists="append")
+    result=result[0:0]
     st.info("1.")
     st.info("2.")
     
