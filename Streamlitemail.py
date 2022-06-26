89980523d64558a5d6944e19835547e0e160d25e
Artuuroos <106031033+Artuuroos@users.noreply.github.com>
2022-06-26 21:48:19 +0200

    Update Streamlitemail.py

--- Streamlitemail.py
+++ Streamlitemail.py
@@ -8,105 +8,21 @@ import requests
 import streamlit as st
 import time
 import yagmail
+import schedule
+import time
 
 
-
-optionen2 = option_menu(menu_title=None,
-                       options=["Home","Diagramm","Price","Notifi", "Preisvorhersage"],
-                       icons=["house","graph-up","clock","alarm", "currency-dollar"],
-                       menu_icon="cast",
-                       default_index=0,
-                       orientation="horizontal",
-                       )
-
-
-if optionen2=="Home":
-    st.write("Homepage")
+def show_name():
     
-else:
-    if optionen2=="Diagramm":
-        with st.sidebar:
-            optionenside= option_menu(menu_title=None,
-            options=["Liniendiagramm","Säulendiagramm"],
-            icons=["graph-up","bar-chart-line"],
-            menu_icon="",
-            default_index=0,
-            orientation="vertical",
-        )
-        if optionenside=="Liniendiagramm":
-            st.write("Hier kommt ein Liniendiagramm hin")
-            with st.form(key='form1'):
-                submit_button = st.form_submit_button(label='Graph ansehen')
-            if submit_button:
-                fig = px.line(        
-                df, #Data Frame
-                x = "Scan", #Columns from the data frame
-                y = "Preis",
-                title = "Preisgraph"
-            )
-                fig.update_traces(line_color = "skyblue")
-                st.plotly_chart(fig)
-           
-        else:
-            st.write("Hier kommt ein Säulendiagramm hin")
-            with st.form(key='form1'):
-                submit_button1 = st.form_submit_button(label='Graph ansehen')
-            if submit_button1:
-                fig = px.bar(        
-                df, #Data Frame
-                x = "Scan", #Columns from the data frame
-                y = "Preis",
-                title = "Preisgraph"
-            )
-                fig.update_traces(line_color = "skyblue")
-                st.plotly_chart(fig) 
-       
-        
-    if optionen2=="Price":
-        st.write("Preisantizipation")
-        
-    if optionen2=="Preisvorhersage":
-        st.open("https://share.streamlit.io/svenpio/digital-lab/Analyse/Preisvorhersage.py")
+    st.info("1.")
+    st.info("2.")
     
-    if optionen2=="Notifi":
-        
-        st.subheader("Benachrichtigung anfordern")
-        emailteil1=st.text_input("Emailnamen eingeben")
-        emaildomains=["@gmail.com","@gmx.de","@web.de"]
+    
+schedule.every(8).seconds.do(show_name)
 
-        option = st.selectbox('Email Domain auswählen', emaildomains)
-        ganzeemail=emailteil1+option
-
-        port = 587  # For starttls
-        smtp_server = "smtp.gmail.com"
-        yag = yagmail.SMTP("dbtickeralert@gmail.com","ujbdfkbgqwbjemrh")
-        contents = [
-        "Ein neuer Preis ihrer Verbindung ist verfuegbar."
-        "\n"
-        "Kaufen Sie sich ein Ticket."
-        "\n"
-
-        "Freundlicher Gruss"
-        "\n"
-        "\n"
-        "DBTickeralert"
-        ]
-        liste=[1,2,3,4,5,6,7,8,9,10]
-
-                
-        preisangabe = st.slider("Ihr gewünschter Höchstpreis:")
-        with st.form(key='form1'):
-                submit_buttonpreis = st.form_submit_button(label='Benachrichtige mich')    
-                if submit_buttonpreis:
-                    st.write("Sie erhalten eine Email Benachrichitung wenn sich der Preis unter",preisangabe ,"€ befindet") 
-                    for i in range(len(liste)):
-                      if liste[i]<=preisangabe:
-                        yag.send(to=ganzeemail,
-                        subject='Neuer Preis',
-                        contents=contents)
-                      else:
-                        if preisangabe>liste[i]:
-                          st.write("Ihre Kaufbereitschaft ist sehr hoch") 
+while 1:
+    schedule.run_pending()
+    time.sleep(1) 
            
                       
         
